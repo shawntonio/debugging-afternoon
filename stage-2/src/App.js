19970af8368e8f37ec99881c131d9d266ed26ec7
Shawn Turley <antonioturley@gmail.com>
2019-04-09 15:43:06 -0600

    debugged stage2

--- stage-2/src/App.js
+++ stage-2/src/App.js
@@ -5,7 +5,7 @@ import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
 
 class App extends Component {
-  constuctor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       products: [],
@@ -21,18 +21,21 @@ class App extends Component {
       .get("https://practiceapi.devmountain.com/products/")
       .then(response => {
         this.setState({
-          products: response
+          products: response.data,
+          cart: []
         });
       });
   }
   addToCart(item) {
+    const newCart = this.state.cart
+    newCart.push(item)
     this.setState({
-      cart: item
+      cart: newCart
     });
   }
   removeFromCart(index) {
-    let cartCopy = this.state.products.slice();
-    cartCopy.splice(index, 1);
+    let cartCopy = this.state.cart.slice();
+    cartCopy.splice(index, 1)
     this.setState({
       cart: cartCopy
     });
